576bb731283f5c01be1af2748e60a46299d391a1
Damir Sharipov <dammer2k@gmail.com>
2017-02-08 16:45:48 +0300

    try debug

--- lib/sidekiq-marshal.rb
+++ lib/sidekiq-marshal.rb
@@ -1,11 +1,18 @@
 require 'sidekiq'
+require 'logger'
 
 module Sidekiq
 
   def self.load_json(string)
     Marshal.load(string)
+  rescue ex
+    log = Logger.new('/tmp/sq_marshal.log')
+    log.info ex.message
+    log.info string.class
+    log.info string.inspect
+    {}
   end
-  
+
   def self.dump_json(object)
     Marshal.dump(object)
   end
